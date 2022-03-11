47144eef4bdfa80301672d80d604326a7b3028cb
Bala Murugan R <bals161616@gmail.com>
2022-03-11 18:09:15 +0530

    home page template

---
+++ src/components/Viewpage.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from "react";
+
+import "./Viewpage.css";
+// import CartContext from "../contexts/CartContext";
+import { Link } from "react-router-dom";
+
+function Viewpage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    try {
+      let res = await fetch(
+        "https://s3-ap-southeast-1.amazonaws.com/he-public-data/studiod9c0baf.json"
+      );
+      let data = await res.json();
+      console.log("data:", data);
+
+      setProducts(data);
+    } catch (e) {
+      console.log("error:", e);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="heading">Jaguvar Music</h1>
+
+      <div className="all-products">
+        {products.map((e, i) =>
+          true ? (
+            <div key={i}>
+              <div className="details">
+                <img src={e.cover_image} alt="" />
+                <p>
+                  <strong>{e.song}</strong>
+                </p>
+                <p>{e.artists}</p>
+              </div>
+              <a href={e.url}>
+                <button className="btn">Play</button>
+              </a>
+            </div>
+          ) : null
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Viewpage;
